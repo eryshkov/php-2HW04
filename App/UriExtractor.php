3691888ecc5ddb099f2413696459437fb5b5ed96
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-20 20:44:25 +0300

    UriExtractor.php is added

---
+++ App/UriExtractor.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+class UriExtractor
+{
+    public function getClassName(): string
+    {
+        $uri = $this->getAddrPart($_SERVER['REQUEST_URI']);
+        $uriParts = explode('/', $uri);
+
+        $cleanURIParts = [];
+        foreach ($uriParts as $part) {
+            if (!isset($part) || '' === $part) {
+                continue;
+            }
+
+            if (is_numeric($part)) {
+                continue;
+            }
+
+            $cleanURIParts[] = ucfirst($part);
+
+        }
+
+        if (empty($cleanURIParts)) {
+            $cleanURIParts[] = 'Index';
+        }
+
+        return '\App\Controllers\\' . implode('\\', $cleanURIParts);
+
+    }
+
+    protected function getAddrPart(string $uri): ?string
+    {
+        $addr = explode('?', $uri);
+
+        if (!empty($addr)) {
+            return reset($addr);
+        }
+
+        return null;
+    }
+}
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 require __DIR__ . '/autoload.php';
 
-$ctrlName = isset($_GET['ctrl']) ? ucfirst($_GET['ctrl']) : 'Index';
-$ctrlClass = '\App\Controllers\\' . $ctrlName;
+//$ctrlName = isset($_GET['ctrl']) ? ucfirst($_GET['ctrl']) : 'Index';
+//$ctrlClass = '\App\Controllers\\' . $ctrlName;
+
+$uriExtractor = new \App\UriExtractor();
+$ctrlClass = $uriExtractor->getClassName();
 
 $ctrl = new $ctrlClass;
 $ctrl->action();
