9ab229bd80ae9553a643b08ad33e6f0afc0f1433
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-20 21:52:01 +0300

    formatting is fixed

--- tests/Db.php
+++ tests/Db.php
@@ -1,12 +1,13 @@
 <?php
 require __DIR__ . '/../autoload.php';
+
 use App\Db;
 
 $db = new Db();
 
 $query = 'UPDATE undefTable SET title=:newTitle WHERE id=:id';
 $params = [
-    ':id' => 1,
+    ':id'       => 1,
     ':newTitle' => 'test',
 ];
 $result = $db->execute($query, $params);
@@ -14,7 +15,7 @@ assert(false === $result);
 
 $query = 'UPDATE news SET title=:newTitle WHERE id=:id';
 $params = [
-    ':id' => 3,
+    ':id'       => 3,
     ':newTitle' => 'Венесуэла выдворила европейского посла',
 ];
 $result = $db->execute($query, $params);
