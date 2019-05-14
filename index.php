603f5fdb3af2ee35b976e3f0c93fed91c4fddd45
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-14 22:03:17 +0300

    php-doc comment at front controller added

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/autoload.php';
 $router = new \App\Router();
 $ctrlClass = $router->getControllerName();
 
+/** @var \App\Controllers\Controller $ctrl */
 $ctrl = new $ctrlClass;
 $ctrl->setParameters($router->getParameters());
 $ctrl->action();
